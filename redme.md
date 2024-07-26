fac83fb04c85c171e62d5a50445783d3ec13efda
Moustapha Sabir <moustaphasabirmoussa61@gmail.com>
2024-07-26 09:19:49 +0200

    blog

---
+++ redme.md
@@ -0,0 +1,2 @@
+Lien cours :
+https://fine-stranger-d13.notion.site/Pr-pa-LEA-Juillet-2024-a99cd5d41fe342a1ae92ae361ffcc6bf
--- tache.sql
+++ tache.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `todo_list`
+-- Base de données : `my-todo-list_todo_list`
 --
 
 -- --------------------------------------------------------
