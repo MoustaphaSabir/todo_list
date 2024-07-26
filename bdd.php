b1812814394e07fea7c2c1e73ee3b39a8755cc12
Moustapha Sabir <moustaphasabirmoussa61@gmail.com>
2024-07-26 09:19:20 +0200

    blog

--- bdd.php
+++ bdd.php
@@ -4,6 +4,13 @@ $dbname = 'todo_list';
 $root = 'root';
 $password = '';
 
+// $host = 'mysql-my-todo-list.alwaysdata.net';
+// $dbname = 'my-todo-list_todo_list';
+
+// $root = '370452';
+// $password = 'Masta?66';
+
+
 try {
     $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $root, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     // echo "Connexion réussie à la base de données.";
